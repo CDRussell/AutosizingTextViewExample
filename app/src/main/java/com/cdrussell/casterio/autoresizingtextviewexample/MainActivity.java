3204e81ffcfb1855568c3982211dcfd35d72112d
Craig Russell <craig.russell.1986@gmail.com>
2017-09-22 00:24:09 +0100

    Add seekbar to dynamically controlling the size of the text views

--- app/src/main/java/com/cdrussell/casterio/autoresizingtextviewexample/MainActivity.java
+++ app/src/main/java/com/cdrussell/casterio/autoresizingtextviewexample/MainActivity.java
@@ -1,7 +1,12 @@
 package com.cdrussell.casterio.autoresizingtextviewexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView shortText = findViewById(R.id.smallLength);
+        final TextView mediumText = findViewById(R.id.mediumLength);
+        final TextView longText = findViewById(R.id.longLength);
+
+        //TextViewCompat.setAutoSizeTextTypeWithDefaults(shortText, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                setHorizontalMargin(shortText, i);
+                setHorizontalMargin(mediumText, i);
+                setHorizontalMargin(longText, i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void setHorizontalMargin(View view, int margin) {
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) view.getLayoutParams();
+        int marginPx = convertDpToPixel(margin);
+        params.setMarginStart(marginPx);
+        params.setMarginEnd(marginPx);
+        view.setLayoutParams(params);
+    }
+
+    private int convertDpToPixel(float dp){
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        return (int) (dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 }
